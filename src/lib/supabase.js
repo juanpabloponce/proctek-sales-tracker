7b5a8c4178868be14b7745990f8408fd531606f1
juanpabloponce <juanpablo.gponce@gmail.com>
2026-01-13 00:39:12 +0400

    Delete src directory

--- src/lib/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://rxbhadpyytdzktwwomxb.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ4YmhhZHB5eXRkemt0d3dvbXhiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgxNjQ5MDcsImV4cCI6MjA4Mzc0MDkwN30.XCaA1UZpkLe9MkdkhHXgythjplbrU11HLrzfZEAbVGU'
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
